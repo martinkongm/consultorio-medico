5554e53edb0e7bf833bfc3aac777acc890c9cb84
Martin <123519281+martinkongm@users.noreply.github.com>
2025-08-03 13:03:04 -0500

    Fixed migracion

--- backend/db/migracion.js
+++ backend/db/migracion.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./clinic.sqlite');
+const db = new sqlite3.Database('../clinic.sqlite');
 
 db.serialize(() => {
   const alter = (sql) => {
